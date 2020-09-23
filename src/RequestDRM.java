ce232b577dc025ae1ec57ad353dd85205d0117df
ychen9 <ychen9@futurewei.com>
2020-09-22 20:45:53 -0700

    1

--- src/RequestDRM.java
+++ src/RequestDRM.java
@@ -3,14 +3,12 @@ import javax.crypto.Mac;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
-import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
 import java.security.spec.AlgorithmParameterSpec;
 import java.time.Duration;
@@ -31,12 +29,13 @@ public class RequestDRM {
         String xtimestamp = Long.toString(new Date().getTime());
         String begin = Long.toString(new Date().getTime()/1000);
         String expire = Long.toString(new Date().getTime()/1000 + 86400);
-        byte[] IV = buildRandomBytes();
-        byte[] key = doCipher(1, IV, Base64.getDecoder().decode(AES), IV);
+        byte[] IV = buildRandomBytes(16);
+        byte[] contentkey = buildRandomBytes(32);
+        byte[] key = doCipher(1, contentkey, Base64.getDecoder().decode(AES), IV);
         String keyString = Base64.getEncoder().encodeToString(key);
 
         String IVString = Base64.getEncoder().encodeToString(IV);
-        String postbody = "{\"type\": \"licenseRequestExt\",\"payload\": \"eyJjZXJ0aWZpY2F0ZUNoYWluIjpbIk1JSUQ1RENDQXN5Z0F3SUJBZ0lVQVFJQUFBQVE5MDQ5N0ZKVUlmc1JWem40dnJvd0RRWUpLb1pJaHZjTkFRRUxCUUF3TWpFTE1Ba0dBMVVFQmd3Q1ZWTXhEVEFMQmdOVkJBb01CRU5FVkVFeEZEQVNCZ05WQkFNTUMwUmxkbWxqWlNCRFFTQXlNQjRYRFRJd01EWXlNakl4TXpNek5Gb1hEVFF3TURZeU1qSXhNek16TkZvd2daSXhDekFKQmdOVkJBWU1Ba05PTVE4d0RRWURWUVFLREFaSWRXRjNaV2t4RXpBUkJnTlZCQXNNQ2toMVlYZGxhU0JEUWtjeEtqQW9CZ05WQkFNTUlVVklMVVJTVFVNdFJFTkVMVVZJTFVoVlFWY3RNakF5TURBeUxUQXlMVUZPUVRFeE1DOEdBMVVFQlF3b01EWTNPVGN3T1dRd01URTBOR1JrWldVelpUaGhaV05tWVRKall6azJaR00wWkdGa1ptTmtaakNDQVNJd0RRWUpLb1pJaHZjTkFRRUJCUUFEZ2dFUEFEQ0NBUW9DZ2dFQkFLRTJDdnZ2cTdpRk9ZMGtWZTRaREIwSVAzNnJjOWErY0xBK3liaTJpTkxPVmhjWWpQSE9Xb1VjdDVoelFLOTV4YnI3TCt2NTBWcXd6L3BSczVRUU5LZTVnQnZzV2NzZGFNS29uSGRoV2o5amFyMWozQ3YyMFN5aThsdi9jTyswTEpIR2ZnNklhbnBjc1FUTUdFNXREWG1hNjdRVE5UTnM1OGZ2VFJ0elNHeVRKUkZmMWIxQVZiWWdkbVlPM3JlemQ1cFZWdzF0SjNuUmVEOXFERXFQM1dVZkhkWEJnTEVlZE9pQ0tTaG52cWxwNCtCOWlXOStGcFRUd1VLRnpyeEs1ZzZTdS9CYlNMT3k1TnBpYmZLcDFIMlRLdXNSSFE2RVpZZ2tuZ2djU0Rhbi9HdFhDb0psdlAxVUU2aGJhR2ZRU3IyeGlTclgzWFMxUk5UZlZPVklXYWNDQXdFQUFhT0JrRENCalRBT0JnTlZIUThCQWY4RUJBTUNCYUF3SHdZRFZSMGpCQmd3Rm9BVVJjYm9SR2JQWHJlZXZkQkRnSVM5VDcvSDZoNHdGUVlEVlIwbEFRSC9CQXN3Q1FZSEtvRWNodTh3QlRCREJnTlZIUjhFUERBNk1EaWlOcVEwTURJeEN6QUpCZ05WQkFZTUFrTk9NUTB3Q3dZRFZRUUtEQVJEUkZSQk1SUXdFZ1lEVlFRRERBdEVaWFpwWTJVZ1EwRWdNVEFOQmdrcWhraUc5dzBCQVFzRkFBT0NBUUVBQjhhdFExeGtKR05Xay8zeEhLeEcrV0lLMXRiQVl0cE82RlM3Z0NtOE1idVlyZDZVbzhocXhsMWZnZTh2M25CdjZqemVKbXN3RjZSbXM5bGV2emU2L1g2YkxldzdmcVdnVG5RTzMzejhMYkFtNWRHL2t3YW5tZjI0akYvRnVKWGlHajloZms3dXRpZWlPcmVZTGJTdmFmVVUzNjVrNENTeW5ieDhPQ1NYMndiZ1k4QVpCSWJFMTB5UXZsWXFyclF0b3l3N3RSTnRWY05YOTZMa1pmMDVPaWROQ0hCZzY1K3ZsajVUblptSVIwR24xemplbVRDNXNWN0hFWHhmdHMxejJKcHJ4VVlTaTFRQjIrQ1M5RCt2VnVEcTd1QWtZbitERGVGdG5vd1Evb1lXSEpOOVVibnJ4VWdIdnVxYXpVU3FmL3ozODlQTS8rUHViUExNMGdiTFlRPT0iLCJNSUlEUmpDQ0FpNmdBd0lCQWdJRVNEbWZSakFOQmdrcWhraUc5dzBCQVFzRkFEQXVNUXN3Q1FZRFZRUUdEQUpEVGpFTk1Bc0dBMVVFQ2d3RVEwUlVRVEVRTUE0R0ExVUVBd3dIVW05dmRDQkRRVEFnRncweU1EQTJNRGN4TmpBd01EQmFHQTh5TURjd01EWXdPREUxTlRrMU9Wb3dNakVMTUFrR0ExVUVCZ3dDVlZNeERUQUxCZ05WQkFvTUJFTkVWRUV4RkRBU0JnTlZCQU1NQzBSbGRtbGpaU0JEUVNBeU1JSUJJakFOQmdrcWhraUc5dzBCQVFFRkFBT0NBUThBTUlJQkNnS0NBUUVBeC9HZHBVakx4WCtPTCtYSlpiOWVYd0FpdnRxWHNPOW95R3BNVFBYR3RVbFZsbDFFbmVDc0k2YU1kNEN2RkhnbDNPcXpsdUVDMWIxcUNVNHZ4dVNOS3ozMUtmUDVIUmV3cGI3Zmd4VkRGVmRDbHFXRGYzTk93SWVtQ2JYQ2ZraFhIc1dJMGh2WkZuVXBrM2wvQitoendHbHNMY3crVkFUMG5OYVovS3BoenFPYS9RZlZVbmFJWG9aYk8waVpwR0FmYkJiYWZtY3c5U0ZXemY2eHE3WHFBdTJ6L2lJSW82NzBlQ3BVWFQxaEpKVmpqRUFKVGZ5c0pVdERrQjRCZWdQNG5EbzFMZGRHdEhBMlFEeFR6NUdETktDUHNZNWJZL1UwRTFmLzFkRDVPaDl2NmVDRVhPQm5vTGRCTDBRQTFudnNVeGhyMWZ3N3FkQVlpUDVTaVc0dUNRSURBUUFCbzJZd1pEQWRCZ05WSFE0RUZnUVVSY2JvUkdiUFhyZWV2ZEJEZ0lTOVQ3L0g2aDR3RGdZRFZSMFBBUUgvQkFRREFnRUdNQklHQTFVZEV3RUIvd1FJTUFZQkFmOENBUUF3SHdZRFZSMGpCQmd3Rm9BVWR6bm90WlhxSVJOSm54RTRpUXY3TUJmRmRSNHdEUVlKS29aSWh2Y05BUUVMQlFBRGdnRUJBRVYrTGdXQkw1QW1sNTlzeXFYbkU0OHQwWEIxOVQ3djNhNXpLZ2YwMmlMVmdpeWVheU1WYWNGdVBCTXd2d0JiOVhqUWVVb09VVzBpNW5GMHk1cXVlS0VvcTBaVW9wbXRveE5EMi82a3loMHorcGRrZE5NK1pEVjI0Ny9OOWlrWTkwbE10WkV6R2Y0bVgydnFlL0pibjRGTnJMMzlxNndhQkdvZjR1V2FnSU9nNDdEVGF5UEprRmtKeHFCWi9TeVRRZFVKTmpNMzAyMjJSQ0JVbDdHem9nNDFMWXQvTlRGL0wrL0xiOHNrNmxUWXNBaXZDLzZZeHVNajhjbUxxb1pYLy80KzRTZGhSOG1tNDNFTlBXYXZXWWM0RGE1Q2xmM3U2cnd3THpLR0w0MVNQZG00VmtIdlp0di9kaHA1TTRjZytpYWhveEQzNHZRWmNsTXdNa0VnYmwwPSJdLCJub25jZSI6IjJuN0Z6RW1xd09mTTNDWXh1UFA2WXc9PSIsInR5cGUiOiJsaWNlbnNlUmVxdWVzdCIsInZlcnNpb24iOiIyLjAiLCJyZXF1ZXN0VGltZSI6IjE1OTk2Nzg2OTMiLCJkZXZpY2VJRCI6IkJubHduUUVVVGQ3ajZLN1Bvc3lXM0UydC9OOD0iLCJzdXBwb3J0ZWRBbGdvcml0aG1zIjpbIktNU1Byb2ZpbGU0Il0sImNvbnRlbnRJRHMiOlsiQUFBQXEzQnpjMmdBQUFBQVBWNXROWnVhUWVpNFE5MDhibkxFTEFBQUFJdDdJbU52Ym5SbGJuUkpSQ0k2SWxac1ZqUlJhM1F5WlVWd1ZrMUZPVk5hYmtreVpXdE9kVTE2U214a2VqQTVJaXdpWlc1elkyaGxiV0VpT2lKRFJVNURJaXdpYTJsa2N5STZXeUpaVjBWM1dtcE9iVTVVWXpSWlZHUnJUa2RGZDA1SFJUTlpNbEV5V2tSU2EwMXFaRzFOZWswelQxUnJQU0pkTENKMlpYSnphVzl1SWpvaVZqRXVNQ0o5Il0sInNpZ25hdHVyZSI6ImVSN2grRXFIenl6MkJoQ0w0UFdILzkzblhrYXhtVlJOUkNVQjNFQ2NpTlcyTWpCa1d0YTRzbTlXdUYxUDVtWTdTTjNRK3Z4OU53V2JRSXpHdkdSbDJ1SVdFYjhvdi81NXBPTFJwTy92UFJCbm5iekZ0SHNGWkJZbDk0NG9IRTVoOGRMQjh1WHhySEZtWm9CazJobW5kbEREc2p1SjFHSy9Jdm16clF3VHFzaXdwTFNUMnVhQUFJbk9RT3hTWkFlMEVSemlIZ1JHMFNHalhySlE4ZnRCbE5PbHB4ZVBmOHV4ZFZQVCswSThPNi85UWt0N0tDeEFSTmNQYkxhSll4cjUveHRyRFVKTUNtU0plbExEa0tEU3RFNU9WeUQzQXU4SzVDdFZiZXcvNVpNTmFoQkROeWZKT0RmQnI0emt0cVZPMGFhTFY4MENyUXprS0pScSs0ZWxKZz09In0=\"," +
+        String postbody = "{\"type\": \"licenseRequestExt\",\"payload\": \"abcd=\"," +
                 "\"authorizeInfo\": [{\"keyAndPolicy\": [{\"distributionMode\": \"VOD\",\"keyInfo\": [{\"keyId\": \"aa0f3f578a7d4a04a7cd6d4d27f33799\",\"key\": \"" +
                 keyString +
                 "\",\"keyEncryptedIV\": \"" +
@@ -66,6 +65,8 @@ public class RequestDRM {
             System.out.println("AES " +AES);
             System.out.println("IV " +IVString);
             System.out.println("sign key " +signKey);
+
+
 
             System.out.println(encryptedWord);
             System.out.println(xtimestamp);
@@ -124,14 +125,14 @@ public class RequestDRM {
 
         // 返回输出的字节数组
     }
-    public static byte[] buildRandomBytes() {
+    public static byte[] buildRandomBytes(int i) {
         // 获取格式化的字节长度信息
 
         // 构造安全随机数对象
         SecureRandom random = getSecureRandom();
 
         // 定义随机字节数组对象
-        byte[] randomBytes = new byte[16];
+        byte[] randomBytes = new byte[i];
 
         // 构建随机字节数
         random.nextBytes(randomBytes);
